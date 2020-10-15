e9589ca9dd66c51d1a35b0bf5ce3f0d12030f4e1
jacklmbrt07 <jacklmbrt07@gmail.com>
2020-10-15 08:27:49 -0700

    ignore

--- 4-kyu/ParseIntReloaded.js
+++ 4-kyu/ParseIntReloaded.js
@@ -16,53 +16,54 @@ All tested numbers are valid, you don't need to validate them
  */
 
 const numStrings = {
-    "zero": 0, //unique
-    "one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7,
-    "eight": 8,
-    "nine": 9,
-    "ten": 10, 
-    "eleven": 11,
-    "twelve": 12,
-    "thirteen": 13,
-    "fourteen": 14,
-    "fifteen": 15,
-    "sixteen": 16,
-    "seventeen": 17,
-    "eighteen": 18,
-    "nineteen": 19,
-    "twenty": 20,
-    "thirty": 30,
-    "forty": 40,
-    "fifty": 50,
-    "sixty": 60,
-    "seventy": 70,
-    "eighty": 80,
-    "ninety": 90,
-    "hundred": 100,
-    "thousand": 1000,
-    "million": 1000000,
-    "and": "+",
-}
+    zero: 0, //unique
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+    ten: 10,
+    eleven: 11,
+    twelve: 12,
+    thirteen: 13,
+    fourteen: 14,
+    fifteen: 15,
+    sixteen: 16,
+    seventeen: 17,
+    eighteen: 18,
+    nineteen: 19,
+    twenty: 20,
+    thirty: 30,
+    forty: 40,
+    fifty: 50,
+    sixty: 60,
+    seventy: 70,
+    eighty: 80,
+    ninety: 90,
+    hundred: 100,
+    thousand: 1000,
+    million: 1000000,
+    and: "+",
+};
 
 function parseInt(string) {
-    var strArr = string.split(" ")
+    var strArr = string.split(" ");
     // first space words are seperated by multiplication
     // second space words are seperated by addition (and)
     // hypen seperated by addition
     return numStrings[string];
-
 }
 
 console.log(parseInt("one"));
 console.log(parseInt("twenty"));
 console.log(parseInt("two hundred forty-six"));
-console.log(parseInt("seven hundred eighty-three thousand nine hundred and nineteen"));// 783919
+console.log(
+    parseInt("seven hundred eighty-three thousand nine hundred and nineteen")
+); // 783919
 //"(((seven*hundred)+(eighty+three))*thousand) + (nine*hundred) + nineteen"
 // (7*100+80+3)*1000+9*100+19 = 783919
 // 2*100+40+6 = 246
