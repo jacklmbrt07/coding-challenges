71fd20509ac5e42439a7c3b33304b86e3a082f57
jacklmbrt07 <jacklmbrt07@gmail.com>
2020-11-20 08:10:31 -0800

    cases created

--- 3-kyu/LastDigit.js
+++ 3-kyu/LastDigit.js
@@ -13,39 +13,76 @@ Corner cases: we assume that 0 ^ 0 = 1 and that lastDigit of an empty list equal
 
 This kata generalizes Last digit of a large number; you may find useful to solve it beforehand.
 
-
+source  from: https://exponentcalculator.net/Tutorials/How-to-find-the-last-digit-of-a-large-exponent.html
  */
 
 function lastDigit(arr) {
-    
-    //[ 1, 2, 3, 4, 5 ] => [1, 2, 3, 1024 ] => [1 ,2 , 3^1024] => 1, infinity = > [infinity]
-    while(arr.length > 1){
-        
-        
-        arr[arr.length-2] = arr[arr.length-2] ** arr[arr.length-1]
-         arr.pop()
+    while (arr.length > 2) {
+        arr[arr.length - 2] = arr[arr.length - 2] ** arr[arr.length - 1];
+        arr.pop();
     }
-    return arr[0]
+    switch (arr[0] % 10) {
+        case 0:
+            return 0;
+        case 1:
+            return 1;
+        case 2:
+            return mod4(arr[1], 6, 2, 4, 8);
+        case 3:
+            return mod4(arr[1], 1, 3, 9, 7);
+        case 4:
+            return mod2(arr[1], 6, 4);
+        case 5:
+            return 5;
+        case 6:
+            return 6;
+        case 7:
+            return mod4(arr[1], 1, 7, 9, 3);
+        case 8:
+            return mod4(arr[1], 6, 8, 4, 2);
+        case 9:
+            return mod2(arr[1], 9, 1);
+    }
 }
 
-console.log(lastDigit([3, 4, 2]))
+function mod2(exp, a, b) {
+    switch (exp % 2) {
+        case 0:
+            return a;
+        case 1:
+            return b;
+    }
+}
 
-// console.log(lastDigit([]));
-// console.log(lastDigit([0, 0])); // 0 ^ 0
-// console.log(lastDigit([0, 0, 0])); // 0^(0 ^ 0) = 0^1 = 0
-// console.log(lastDigit([1, 2]));
-// console.log(lastDigit([3, 4, 5]));
-// console.log(lastDigit([4, 3, 6]));
-// console.log(lastDigit([7, 6, 21]));
-// console.log(lastDigit([12, 30, 21]));
-// console.log(lastDigit([2, 2, 2, 0]));
-// console.log(lastDigit([937640, 767456, 981242]));
-// console.log(lastDigit([123232, 694022, 140249]));
-// console.log(lastDigit([499942, 898102, 846073]));
+function mod4(exp, a, b, c, d) {
+    switch (exp % 4) {
+        case 0:
+            return a;
+        case 1:
+            return b;
+        case 2:
+            return c;
+        case 3:
+            return d;
+    }
+}
 
-// var r1 = Math.floor(Math.random() * 100);
-// var r2 = Math.floor(Math.random() * 10);
+console.log(lastDigit([]));
+console.log(lastDigit([0, 0])); // 0 ^ 0
+console.log(lastDigit([0, 0, 0])); // 0^(0 ^ 0) = 0^1 = 0
+console.log(lastDigit([1, 2]));
+console.log(lastDigit([3, 4, 5]));
+console.log(lastDigit([4, 3, 6]));
+console.log(lastDigit([7, 6, 21]));
+console.log(lastDigit([12, 30, 21]));
+console.log(lastDigit([2, 2, 2, 0]));
+console.log(lastDigit([937640, 767456, 981242]));
+console.log(lastDigit([123232, 694022, 140249]));
+console.log(lastDigit([499942, 898102, 846073]));
 
-// console.log(lastDigit([]));
-// console.log(lastDigit([r1]));
-// console.log(lastDigit([r1, r2]));
+var r1 = Math.floor(Math.random() * 100);
+var r2 = Math.floor(Math.random() * 10);
+
+console.log(lastDigit([]));
+console.log(lastDigit([r1]));
+console.log(lastDigit([r1, r2]));
