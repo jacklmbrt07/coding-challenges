3227ff4169323d56479b22a2f46aeea9a26ac0c1
jacklmbrt07 <jacklmbrt07@gmail.com>
2020-11-23 07:44:58 -0800

    findbattleship

--- 3-kyu/BattleshipValidator.js
+++ 3-kyu/BattleshipValidator.js
@@ -1,21 +1,63 @@
 function validateBattlefield(field) {
+    const transposeField = field;
     const ships = [
-        { name: "battleship", size: 4, qty: 1 },
+        {
+            name: "battleship",
+            size: 4,
+            qty: 1,
+            coordinates: findBattleship(field),
+        },
         { name: "cruiser", size: 3, qty: 2 },
         { name: "destroyer", size: 2, qty: 3 },
         { name: "submarine", size: 1, qty: 4 },
     ];
     checkSum = (field) => {
-        var sum = 0
-        for (let i = 0, ; i < field.length; i++) {
+        var sum = 0;
+        for (let i = 0; i < field.length; i++) {
             sum = sum + field[i].reduce(a, (b) => a + b);
         }
         if (sum != 20) return false;
         return true;
-    }
-    checkBattleship = (field) => {
-
-    } 
+    };
+    findBattleship = (field) => {
+        // horizontal
+        for (let i = 0; i < field.length; i++) {
+            for (let j = 0; j < field[i]; j++) {
+                if (
+                    field[i][j] === field[i][j + 1] &&
+                    field[i][j] == field[i][j + 2] &&
+                    field[i][j] == field[i][j + 3]
+                ) {
+                    return [
+                        [i, j],
+                        [i, j + 1],
+                        [i, j + 2],
+                        [i, j + 3],
+                    ];
+                }
+            }
+        }
+        //vertical
+        field[0].map((_, colIndex) => field.map((row) => row[colIndex])); // transpose the array
+        for (let i = 0; i < field.length; i++) {
+            for (let j = 0; j < field[i]; j++) {
+                if (
+                    field[i][j] === field[i][j + 1] &&
+                    field[i][j] == field[i][j + 2] &&
+                    field[i][j] == field[i][j + 3]
+                ) {
+                    return [
+                        [j, i],
+                        [j + 1, i],
+                        [j + 2, i],
+                        [j + 3, i],
+                    ];
+                }
+            }
+        }
+        return false;
+    };
+    findCruiser = (field) => {};
 }
 
 console.log(
