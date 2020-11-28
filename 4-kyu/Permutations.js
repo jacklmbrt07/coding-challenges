8ee325684c39dc753d8fc3a2d8420547f20f96d3
jacklmbrt07 <jacklmbrt07@gmail.com>
2020-11-28 07:21:33 -0800

    discovered permutation formula

--- 4-kyu/Permutations.js
+++ 4-kyu/Permutations.js
@@ -10,6 +10,32 @@ permutations('aabb'); // ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
 The order of the permutations doesn't matter.
  */
 
-function permutations(string) {}
+function permutations(string) {
+    var arr = [];
 
-console.log(permutations("a"), permutations("ab"), permutations("aabb"));
+    const stringArr = string.split("");
+    const numUnique = [...new Set(stringArr)];
+    console.log(numUnique);
+
+    function factorial(n) {
+        return n ? n * factorial(n - 1) : 1;
+    }
+
+    const numOfPermutations = // P = n! / (u1!*u2!...un!)
+        /**
+         * n = total number of letters
+         * r = number of unique lterrs
+         *
+         */
+
+        arr.push(stringArr);
+    return arr[0];
+}
+
+console.log(
+    permutations("a"), // 1
+    permutations("ab"), // 2
+    permutations("abc"), // 6
+    permutations("aabb"), // 6
+    permutations("banana") // 60
+);
