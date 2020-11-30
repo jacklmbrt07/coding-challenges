3d163629a4dbb2abbc37e13a87c879b6eeaf0ba9
jacklmbrt07 <jacklmbrt07@gmail.com>
2020-11-30 06:44:49 -0800

    python

---
+++ Python/8-kyu/Maps.py
@@ -0,0 +1,15 @@
+# Given an array of integers, return a new array with each value doubled.
+
+# For example:
+
+# [1, 2, 3] --> [2, 4, 6]
+
+# For the beginner, try to use the map method - it comes in very handy quite a lot so is a good one to know.
+def double(number):
+    return number * 2
+
+def maps(a):
+    doubled = map(double, a)
+    return doubled
+    
+print(maps([1, 2, 3]))
